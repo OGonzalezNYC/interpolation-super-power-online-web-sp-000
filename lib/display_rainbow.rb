a67708a7932a070f73b4b1617a5db41e2e0d83bd
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-02 10:03:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,5 +4,5 @@
 #   puts "R: #{array[0]}, O: #{array[1]}, Y: #{array[2]}, G: #{array[3]}, B: #{array[4]}, I: #{array[5]}, V: #{array[6]}"
 # end
 
-def display_rainbow
+def display_rainbow(array_of_colors)
 end
